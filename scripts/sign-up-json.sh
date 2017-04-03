5ccb221d9e6b9c952f9590d248832f179bd3e2a2
antleo1995 <antleo1995@gmail.com>
2017-04-03 10:15:53 -0400

    Sign Up Curl
    Created a curl without variables

--- scripts/sign-up-json.sh
+++ scripts/sign-up-json.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
 #curl "http://localhost:3000/sign-up" \
-curl "http://httpbin.org/post" \
-  --include \
-  --request POST \
+curl --include --request POST http://localhost:4741/sign-up \
   --header "Content-Type: application/json" \
-  --data ""
+  --data '{
+    "credentials": {
+      "email": "an@example.email",
+      "password": "an example password",
+      "password_confirmation": "an example password"
+    }
+  }'
 
+
+/*
+  API="${API_ORIGIN:-http://localhost:4741}"
+  URL_PATH="/sign-up"
+  curl "${API}${URL_PATH}" \
+    --include \
+    --request POST \
+    --header "Content-Type: application/json" \
+    --data '{
+      "credentials": {
+        "email": "'"${EMAIL}"'",
+        "password": "'"${PASSWORD}"'",
+        "password_confirmation": "'"${PASSWORD}"'"
+      }
+    }'
+*/
 # data output from curl doesn't have a trailing newline
 echo
