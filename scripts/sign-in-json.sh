0452b1e221e1eda475fc862012916b9c2fcd0ac8
antleo1995 <antleo1995@gmail.com>
2017-04-03 11:45:20 -0400

    API Token Auth
    Got sign in curl script working. Sign up feature is complete.

--- scripts/sign-in-json.sh
+++ scripts/sign-in-json.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 #curl "http://localhost:3000/sign-in" \
-curl "http://httpbin.org/post" \
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/sign-in"
+curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-  --data ""
-
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
 # data output from curl doesn't have a trailing newline
 echo
